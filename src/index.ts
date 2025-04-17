56c8fdbd9452baf20ca3402dae2991182ef763e0
JoShMiQueL <joshmiquel.cs@gmail.com>
2025-04-17 04:20:57 +0200

    feat: refactor exports and add default export to src/index.ts [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-export * from './memory';
-export * from './process';
+import { readMemory, writeMemory } from './memory';
+import { openProcess, closeProcess, type ProcessObject } from './process';
+
+export {
+  readMemory,
+  writeMemory,
+  openProcess,
+  closeProcess,
+  type ProcessObject
+};
+
+const memoryprocess = {
+  readMemory,
+  writeMemory,
+  openProcess,
+  closeProcess
+};
+
+export default memoryprocess;
